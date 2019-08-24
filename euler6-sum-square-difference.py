87e26ccd1e669f28d62f7cd507e854bc9925f158
Robin Neville <byntje@gmail.com>
2019-08-24 11:29:32 +0200

    Replaced odd character in euler6.

--- euler6-sum-square-difference.py
+++ euler6-sum-square-difference.py
@@ -6,14 +6,14 @@ The square of the sum of the first ten natural numbers is,
 
 (1 + 2 + ... + 10)**2 = 552 = 3025
 Hence the difference between the sum of the squares of the first ten natural numbers
-and the square of the sum is 3025 − 385 = 2640.
+and the square of the sum is 3025 - 385 = 2640.
 
 Find the difference between the sum of the squares of the first one hundred natural
 numbers and the square of the sum.
 """
 
 N = 100
-natural = range(1,N+1)
+natural = range(1, N+1)
 sum_of_squares = sum(n**2 for n in natural)
 square_of_sums = sum(natural)**2
 
