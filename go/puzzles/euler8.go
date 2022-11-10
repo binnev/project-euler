70408141e4491d25ce4db514a0a9b1b2e2783deb
binnev <byntje@gmail.com>
2022-11-10 13:26:14 +0100

    new euler8.go approach

--- go/puzzles/euler8.go
+++ go/puzzles/euler8.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-func Euler8() string {
+func euler8PythonStyle() string {
 	numStr := strings.Join(
 		strings.Fields(utils.LoadPuzzleInput("euler8")),
 		"",
@@ -27,4 +27,30 @@ func Euler8() string {
 	}
 
 	return fmt.Sprint(utils.MaxInt(products))
+}
+
+func euler8GoStyle() string {
+	numStr := strings.Join(
+		strings.Fields(utils.LoadPuzzleInput("euler8")),
+		"",
+	)
+	length := 13
+	maxProd := 0
+	for ii := 0; ii < len(numStr)-1-length; ii++ {
+		subStr := numStr[ii : ii+length]
+		prod := 1
+		for _, digitStr := range subStr {
+			digit, _ := strconv.Atoi(string(digitStr))
+			prod *= digit
+		}
+		if prod > maxProd {
+			maxProd = prod
+		}
+	}
+
+	return fmt.Sprint(maxProd)
+}
+
+func Euler8() string {
+	return euler8GoStyle()
 }
